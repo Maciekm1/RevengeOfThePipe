4b903452bb20e371b65261e0c07299ffc43dc6c5
Maciej Zaweracz <maciekm1pro@gmail.com>
2023-11-18 15:40:31 +0000

    Project Update

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-var score: int = 0
+var score: int = 0:
+	set(value):
+		score = value
+		score_label.text = str(score)
 var game_time: float = 0
 
 @onready var player: Player = $Player
+@onready var score_label = $UI/LabelMarginContainer/Score
 
 func _ready():
 	pass
@@ -13,3 +17,5 @@ func _process(delta):
 	
 func reset():
 	player.reset()
+	score = 0
+	game_time = 0
